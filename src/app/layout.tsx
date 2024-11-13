69620165f69e55bfe6f4f6d6d55cd7341e292a95
Sebastian <sebastianporttka@gmail.com>
2024-11-13 04:38:35 +0100

    analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Nav from "@/components/global/Nav/Nav";
 import { Hind } from "next/font/google";
 import CookieConsentBanner from "@/components/CookieConsent/CookieConsentBanner";
+import Script from "next/script";
 
 const hind = Hind({
 	subsets: ["latin"],
@@ -49,11 +50,33 @@ export default function RootLayout({
 					rel='stylesheet'
 					href='https://fonts.googleapis.com/css2?family=Hind:wght@400;700&family=Proza+Libre:wght@400;700&display=swap'
 				/>
+				<meta
+					name='google-site-verification'
+					content='GJ7zeW_Gr1oZJG0-ve3_v6O3Te63hkGs57X7rmGv8YQ'
+				/>
 			</head>
 
 			<body className={hind.className}>
-				<Nav />
+				<Script
+					strategy='afterInteractive'
+					src='https://www.googletagmanager.com/gtag/js?id=G-6RLN8N2PD3'
+				/>
+				<Script
+					id='google-analytics'
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-6RLN8N2PD3', {
+        page_path: window.location.pathname,
+      });
+    `,
+					}}
+				/>
 
+				<Nav />
 				{children}
 				<CookieConsentBanner />
 			</body>
