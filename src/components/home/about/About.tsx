d0647a8626c0362c96584c0f1f8e61f433166ff5
Sebastian <sebastianporttka@gmail.com>
2024-11-14 00:12:07 +0100

    about padding test

--- src/components/home/about/About.tsx
+++ src/components/home/about/About.tsx
@@ -13,7 +13,7 @@ const About = () => {
 					backgroundImage: "url('/assets/img/Dewwwelopment-projektowanie.jpg'",
 				}}
 			>
-				<article className='relative w-[90%] min-h-[90svh]  z-30 flex flex-col justify-evenly gap-8 sm:w-[80%] lg:w-[80%] lg:py-12 xl:max-w-[1000px]'>
+				<article className='relative w-[90%] min-h-[90svh]  z-30 flex flex-col py-6 justify-evenly gap-4 sm:gap-6 sm:w-[80%] lg:w-[80%] lg:py-12 xl:max-w-[1000px]'>
 					<div className='flex items-center justify-between'>
 						<div className='w-[30%]'>
 							<Divider
