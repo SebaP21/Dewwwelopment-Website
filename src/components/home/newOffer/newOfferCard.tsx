42fa6db237b992bbda56bd129dd5641b9f4fcc70
Sebastian <sebastianporttka@gmail.com>
2025-04-22 11:05:19 +0200

    useRef deleted

--- src/components/home/newOffer/newOfferCard.tsx
+++ src/components/home/newOffer/newOfferCard.tsx
@@ -4,7 +4,7 @@
 
 // import Image from "next/image";
 import { useTransform, motion } from "framer-motion";
-import { FC, ReactNode, useRef } from "react";
+import { FC, ReactNode } from "react";
 
 type NewCardProps = {
 	i: number;
