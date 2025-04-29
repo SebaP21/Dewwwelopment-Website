1fa1f072f598c72b1ca0df9f3dcbec6771297fbf
Sebastian <sebastianporttka@gmail.com>
2025-04-29 13:57:49 +0200

    portfolio use spring mass test

--- src/components/Animations/PortfolioGallery/PortfolioGallery.tsx
+++ src/components/Animations/PortfolioGallery/PortfolioGallery.tsx
@@ -12,7 +12,20 @@ import {
 import Image from "next/image";
 import { useState, useLayoutEffect, useRef } from "react";
 
-const images = Array(12).fill("ptak.webp");
+const images = [
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+	"ptak.webp",
+];
 
 type ColumnProps = {
 	images: string[];
@@ -41,32 +54,23 @@ const PortfolioGallery = () => {
 	const { height } = dimension;
 
 	const springConfig = {
-		stiffness: 50,
-		damping: 20,
+		stiffness: 200,
+		damping: 50,
 		mass: 0.7,
 	};
-	//   const springConfig = {
-	// 	stiffness: 18,
-	// 	damping: 22,
-	// 	restSpeed: 0.2,
-	// 	restDelta: 0.2,
-	//   };
 
 	const y = useSpring(
-		useTransform(scrollYProgress, [0, 1], [0, height * 2]),
+		useTransform(scrollYProgress, [0, 1], [0, height * 2.25]),
 		springConfig
 	);
-
 	const y2 = useSpring(
 		useTransform(scrollYProgress, [0, 1], [0, height * 3.3]),
 		springConfig
 	);
-
 	const y3 = useSpring(
 		useTransform(scrollYProgress, [0, 1], [0, height * 1.25]),
 		springConfig
 	);
-
 	const y4 = useSpring(
 		useTransform(scrollYProgress, [0, 1], [0, height * 3]),
 		springConfig
@@ -80,22 +84,22 @@ const PortfolioGallery = () => {
 			className='h-[175vh] bg-black flex gap-[2vw] p-[2vw] box-border overflow-hidden'
 		>
 			<Column
-				images={images.slice(0, 3)}
+				images={[images[0], images[1], images[2]]}
 				y={y}
 				extraClass='-top-[45%]'
 			/>
 			<Column
-				images={images.slice(3, 6)}
+				images={[images[3], images[4], images[5]]}
 				y={y2}
 				extraClass='-top-[95%]'
 			/>
 			<Column
-				images={images.slice(6, 9)}
+				images={[images[6], images[7], images[8]]}
 				y={y3}
 				extraClass='-top-[45%]'
 			/>
 			<Column
-				images={images.slice(9, 12)}
+				images={[images[9], images[10], images[11]]}
 				y={y4}
 				extraClass='-top-[75%] hidden md:flex'
 			/>
@@ -138,6 +142,202 @@ const Column: React.FC<ColumnProps> = ({ images, y, extraClass = "" }) => {
 		</motion.div>
 	);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use client";
+
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+
+// import {
+// 	motion,
+// 	useScroll,
+// 	useTransform,
+// 	useSpring,
+// 	useInView,
+// } from "framer-motion";
+// import Image from "next/image";
+// import { useState, useLayoutEffect, useRef } from "react";
+
+// const images = Array(12).fill("ptak.webp");
+
+// type ColumnProps = {
+// 	images: string[];
+// 	y: any;
+// 	extraClass?: string;
+// };
+
+// const PortfolioGallery = () => {
+// 	const gallery = useRef<HTMLDivElement | null>(null);
+// 	const [dimension, setDimension] = useState({ width: 0, height: 0 });
+
+// 	const { scrollYProgress } = useScroll({
+// 		target: gallery,
+// 		offset: ["start end", "end start"],
+// 	});
+
+// 	useLayoutEffect(() => {
+// 		const resize = () => {
+// 			setDimension({ width: window.innerWidth, height: window.innerHeight });
+// 		};
+// 		resize();
+// 		window.addEventListener("resize", resize);
+// 		return () => window.removeEventListener("resize", resize);
+// 	}, []);
+
+// 	const { height } = dimension;
+
+// 	// const springConfig = {
+// 	// 	stiffness: 50,
+// 	// 	damping: 20,
+// 	// 	mass: 0.7,
+// 	// };
+
+// 	//   const springConfig = {
+// 	// 	stiffness: 18,
+// 	// 	damping: 22,
+// 	// 	restSpeed: 0.2,
+// 	// 	restDelta: 0.2,
+// 	//   };
+
+// 	const springConfig = {
+// 		stiffness: 20,
+// 		damping: 26,
+// 		mass: 0.8,
+// 		restSpeed: 0.1,
+// 		restDelta: 0.1,
+// 	  };
+
+// 	const y = useSpring(
+// 		useTransform(scrollYProgress, [0, 1], [0, height * 2]),
+// 		springConfig
+// 	);
+
+// 	const y2 = useSpring(
+// 		useTransform(scrollYProgress, [0, 1], [0, height * 3.3]),
+// 		springConfig
+// 	);
+
+// 	const y3 = useSpring(
+// 		useTransform(scrollYProgress, [0, 1], [0, height * 1.25]),
+// 		springConfig
+// 	);
+
+// 	const y4 = useSpring(
+// 		useTransform(scrollYProgress, [0, 1], [0, height * 3]),
+// 		springConfig
+// 	);
+
+// 	return (
+// 		<motion.div
+// 			id='portfolio'
+// 			ref={gallery}
+// 			layout
+// 			className='h-[175vh] bg-black flex gap-[2vw] p-[2vw] box-border overflow-hidden'
+// 		>
+// 			<Column
+// 				images={images.slice(0, 3)}
+// 				y={y}
+// 				extraClass='-top-[45%]'
+// 			/>
+// 			<Column
+// 				images={images.slice(3, 6)}
+// 				y={y2}
+// 				extraClass='-top-[95%]'
+// 			/>
+// 			<Column
+// 				images={images.slice(6, 9)}
+// 				y={y3}
+// 				extraClass='-top-[45%]'
+// 			/>
+// 			<Column
+// 				images={images.slice(9, 12)}
+// 				y={y4}
+// 				extraClass='-top-[75%] hidden md:flex'
+// 			/>
+// 		</motion.div>
+// 	);
+// };
+
+// export default PortfolioGallery;
+
+// const Column: React.FC<ColumnProps> = ({ images, y, extraClass = "" }) => {
+// 	const ref = useRef(null);
+// 	const isInView = useInView(ref, {
+// 		margin: "0px 0px -20% 0px",
+// 		once: false,
+// 	});
+
+// 	return (
+// 		<motion.div
+// 			ref={ref}
+// 			className={`relative h-full w-1/3 flex flex-col gap-[2vw] ${extraClass}`}
+// 			style={{
+// 				y,
+// 				willChange: isInView ? "transform" : "auto",
+// 			}}
+// 		>
+// 			{images.map((src, i) => (
+// 				<div
+// 					key={i}
+// 					className='h-full w-full relative rounded-[1vw] overflow-hidden'
+// 				>
+// 					<Image
+// 						src={`/assets/img/${src}`}
+// 						alt='image'
+// 						fill
+// 						className='object-cover'
+// 						sizes='(max-width: 768px) 100vw, 33vw'
+// 					/>
+// 				</div>
+// 			))}
+// 		</motion.div>
+// 	);
+// };
 
 // "use client";
 
